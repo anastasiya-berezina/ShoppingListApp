c0452e81c7952338c3c1090e2b526ed2ae6ad377
anastasiya-berezina <anastasiya.berezina@ict.nl>
2026-01-14 11:35:50 +0100

    Add hardcoded shopping list screen

--- lib/screens/shopping_list.dart
+++ lib/screens/shopping_list.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/dummy_items.dart';
 
 class ShoppingList extends StatelessWidget {
+  const ShoppingList({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Shopping List'),
-      ),
-      body: const Center(
-        child: Text('Your shopping list is empty.'),
+      appBar: AppBar(title: const Text('Shopping List')),
+      body: Column(
+        children: [
+          for (final item in groceryItems) 
+          Container(
+            width: double.infinity,
+            height: 50,   
+            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+            child: Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: item.category.color,
+                ),
+                const SizedBox(width: 15),
+                Text(item.name),
+                const Spacer(),
+                Text('${item.quantity}'),
+              ]             
+            ),
+          )
+        ],
       ),
     );
   }
